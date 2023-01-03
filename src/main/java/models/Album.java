9cdf9be12c3e536b93093727fcabbc3fa5fd217a
Aldanis Vigo <aldanisvigo@gmail.com>
2023-01-03 02:13:40 -0600

    added the album serializable model javabean

---
+++ src/main/java/models/Album.java
@@ -0,0 +1,44 @@
+package models;
+
+import java.io.Serializable;
+public class Album implements Serializable {
+    private int id;
+    private String artist;
+    private String name;
+    private String release_date;
+    private double sales;
+    private String genre;
+
+    public Album(int id, String artist, String name, String release_date, double sales, String genre){
+        this.id = id;
+        this.artist = artist;
+        this.name = name;
+        this.release_date = release_date;
+        this.sales = sales;
+        this.genre = genre;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getArtist() {
+        return this.artist;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getReleaseDate() {
+        return this.release_date;
+    }
+
+    public double getSales() {
+        return this.sales;
+    }
+
+    public String getGenre() {
+        return this.genre;
+    }
+}
